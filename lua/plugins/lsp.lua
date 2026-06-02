f1df86e381b9e91c576ac4e05efcde67a9a27364
Gerardo Garcia <headlessflowerdev@gmail.com>
2026-06-01 20:42:31 -0700

    fixed vue_ls error message

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
     cmd = "Mason",
     opts = {},
   },
+
   {
     "mason-org/mason-lspconfig.nvim",
     dependencies = {
@@ -20,11 +21,15 @@ return {
         "bashls",
         "emmet_language_server",
         "tailwindcss",
-        "vue_ls",
         "vtsls",
+        "vue_ls",
       },
+
+      -- We manually enable them below so we control the config order.
+      automatic_enable = false,
     },
   },
+
   {
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
@@ -33,26 +38,35 @@ return {
       "mason-org/mason.nvim",
       "mason-org/mason-lspconfig.nvim",
     },
+
     config = function()
       local capabilities = require("blink.cmp").get_lsp_capabilities()
-      local lspconfig = require("lspconfig")
 
-      local on_attach = function(_, bufnr)
-        local map = function(mode, lhs, rhs, desc)
-          vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
-        end
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("webvim-lsp-attach", { clear = true }),
+        callback = function(event)
+          local bufnr = event.buf
 
-        map("n", "gd", vim.lsp.buf.definition, "Go to definition")
-        map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
-        map("n", "gR", vim.lsp.buf.references, "References")
-        map("n", "gi", vim.lsp.buf.implementation, "Implementation")
-        map("n", "K", vim.lsp.buf.hover, "Hover")
-        map("n", "<leader>lr", vim.lsp.buf.rename, "Rename")
-        map("n", "<leader>la", vim.lsp.buf.code_action, "Code action")
-      end
+          local map = function(mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, {
+              buffer = bufnr,
+              desc = desc,
+            })
+          end
+
+          map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+          map("n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+          map("n", "gR", vim.lsp.buf.references, "References")
+          map("n", "gi", vim.lsp.buf.implementation, "Implementation")
+          map("n", "K", vim.lsp.buf.hover, "Hover")
+          map("n", "<leader>lr", vim.lsp.buf.rename, "Rename")
+          map("n", "<leader>la", vim.lsp.buf.code_action, "Code action")
+        end,
+      })
 
       local vue_language_server_path =
-        vim.fn.stdpath("data") .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+          vim.fn.stdpath("data")
+          .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
 
       local vue_plugin = {
         name = "@vue/typescript-plugin",
@@ -61,52 +75,85 @@ return {
         configNamespace = "typescript",
       }
 
-      local servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              diagnostics = { globals = { "vim" } },
-              workspace = { checkThirdParty = false },
+      vim.lsp.config("lua_ls", {
+        capabilities = capabilities,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              checkThirdParty = false,
             },
           },
         },
-        html = {},
-        cssls = {},
-        jsonls = {},
-        yamlls = {},
-        bashls = {},
-        emmet_language_server = {},
-        tailwindcss = {},
-        vue_ls = {},
-        vtsls = {
-          settings = {
-            vtsls = {
-              tsserver = {
-                globalPlugins = { vue_plugin },
+      })
+
+      vim.lsp.config("html", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("cssls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("jsonls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("yamlls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("bashls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("emmet_language_server", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("tailwindcss", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.config("vtsls", {
+        capabilities = capabilities,
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "javascript.jsx",
+          "typescript",
+          "typescriptreact",
+          "typescript.tsx",
+          "vue",
+        },
+        settings = {
+          vtsls = {
+            tsserver = {
+              globalPlugins = {
+                vue_plugin,
               },
             },
           },
-          filetypes = {
-            "javascript",
-            "javascriptreact",
-            "javascript.jsx",
-            "typescript",
-            "typescriptreact",
-            "typescript.tsx",
-            "vue",
-          },
         },
-      }
+      })
 
-      require("mason-lspconfig").setup({
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            server.capabilities = capabilities
-            server.on_attach = on_attach
-            lspconfig[server_name].setup(server)
-          end,
-        },
+      vim.lsp.config("vue_ls", {
+        capabilities = capabilities,
+      })
+
+      vim.lsp.enable({
+        "lua_ls",
+        "html",
+        "cssls",
+        "jsonls",
+        "yamlls",
+        "bashls",
+        "emmet_language_server",
+        "tailwindcss",
+        "vtsls",
+        "vue_ls",
       })
     end,
   },
